23b840d564357793da992864236e26b4239c8631
Sean Gillies <sean.gillies@gmail.com>
2016-03-27 18:23:55 -0600

    Statically link GEOS

---
+++ hooks.py
@@ -0,0 +1,11 @@
+import os.path
+
+def staticgeos(options, buildout, environment):
+    optsfile = os.path.join(options['compile-directory'], 'gdal-1.11.3', 'GDALmake.opt')
+    geosdir = os.path.join(buildout['buildout']['directory'], 'parts', 'geos')
+    with open(optsfile) as src:
+        options = src.read()
+    repls = '{0}/lib/libgeos.a {0}/lib/libgeos_c.a'.format(geosdir)
+    options = options.replace('-lgeos_c', repls)
+    with open(optsfile, 'w') as dst:
+        dst.write(options)
